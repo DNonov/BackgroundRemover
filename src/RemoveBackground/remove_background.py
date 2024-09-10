b3638d24bea83ac7184e7532c10198561e4dafdf
Dimitar Nonov <d.nonov@gmail.com>
2024-09-11 00:39:42 +0300

    Add python

---
+++ src/RemoveBackground/remove_background.py
@@ -0,0 +1,15 @@
+# from rembg import remove, sessions
+# from PIL import Image
+# import sys
+
+print("hello")
+# input_path = sys.argv[1]
+#
+# output_path = 'output.png'
+#
+# print("Open file: " + input_path)
+# input = Image.open(input_path)
+# print("Remove background: " + input_path)
+# output = remove(input)
+# print("Save file: " + output_path)
+# output.save(output_path)
--- src/Services/System.php
+++ src/Services/System.php
@@ -4,8 +4,9 @@ namespace Dnonov\BackgroundRemover\Services;
 class System {
     static public function executeCommand() {
         $descriptorspec = array(0 => array("pipe", "r"), 1 => array("pipe", "w"));
+        $pathToRemoveBackground = base_path() . "/vendor/dnonov/background-remover/src/RemoveBackground/venv";
 
-        $fullCommand = "cd " . base_path() . "; pwd";
+        $fullCommand = $pathToRemoveBackground . "/venv/bin/python " . $pathToRemoveBackground . "/remove_background.py";
             // base_path() . "/remove_background/venv/bin/python "
             // . base_path() . "/remove_background/remove_background.py "
             // . base_path() . "/remove_background/input3.JPG";
