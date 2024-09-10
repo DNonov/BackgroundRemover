e2b0a99837c57ce19cc39eb093a18d6a225ff49f
Dimitar Nonov <d.nonov@gmail.com>
2024-09-11 00:18:33 +0300

    Fix process and close after read of stdOut

--- src/Services/System.php
+++ src/Services/System.php
@@ -5,13 +5,15 @@ class System {
     static public function executeCommand() {
         $descriptorspec = array(0 => array("pipe", "r"), 1 => array("pipe", "w"));
 
-        $fullCommand = "cd " . base_path() . "; pwd;";
+        $fullCommand = "cd " . base_path() . "; pwd";
             // base_path() . "/remove_background/venv/bin/python "
             // . base_path() . "/remove_background/remove_background.py "
             // . base_path() . "/remove_background/input3.JPG";
 
-        proc_open($fullCommand, $descriptorspec, $pipes);
+        $process = proc_open($fullCommand, $descriptorspec, $pipes);
         $stdOutput = stream_get_contents($pipes[1]);
+
+        fclose($pipes[1]);
         return $stdOutput;
     }
 }
